f67824af97ff831f6351d8a1232ba776359a11e3
Low Kian Seong <kianseong@gmail.com>
2014-09-06 08:43:18 +0800

    Corrected url and added description to setup.py + pep8

--- flexible_form.py
+++ flexible_form.py
@@ -1,9 +1,11 @@
+
 class FlexibleForm:
     help_texts = {}
+
     def replace_help_texts(self, help_texts=None):
         if not help_texts:
             help_texts = self.help_texts
-            
+
         for field, help_text in help_texts.iteritems():
             try:
                 self.fields[field].help_text = help_text
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-flexible-form",
-    version = "1.0.0",
-    url = 'http://ondrejsika.com/docs/django-flexible-form',
-    download_url = 'https://github.com/sikaondrej/django-flexible-form',
-    license = 'GNU LGPL v.3',
-    description = "",
-    author = 'Ondrej Sika',
-    author_email = 'ondrej@ondrejsika.com',
-    py_modules = ["flexible_form"],
-    #packages = find_packages(),
+    name="django-flexible-form",
+    version="1.0.0",
+    url='https://github.com/sika-others/django-flexible-form',
+    download_url='https://github.com/sikaondrej/django-flexible-form',
+    license='GNU LGPL v.3',
+    description="Django form utils",
+    author='Ondrej Sika',
+    author_email='ondrej@ondrejsika.com',
+    py_modules=["flexible_form"],
+    #packages=find_packages(),
 )
